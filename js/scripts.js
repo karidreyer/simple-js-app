118a79e353a3d666386fd924c368f81c3411282e
Kari <kari.lynn.dreyer@gmail.com>
2024-03-26 12:38:09 +0100

    Added Pokemon dataset

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+let pokemonList=[
+    {name: 'Bulbasaur', height: 7, types: ['grass','poison']},
+    {name: 'Ivysaur', height: 10, types: ['grass','poison']},
+    {name: 'Venusaur', height: 20, types: ['grass','poison']},
+    {name: 'Charmander', height: 6, types: 'fire'},
+    {name: 'Charmeleon', height: 11, types: 'fire'},
+    {name: 'Charizard', height: 17, types: ['fire','flying']},
+    {name: 'Squirtle', height: 5, types: 'water'},
+    {name: 'Wartortle', height: 10, types: 'water'},
+    {name: 'Blastoise', height: 16, types: 'water'},
+];

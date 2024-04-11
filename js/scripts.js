e1f1a4f1dd3b4ab2708b040636f657f814abe75c
Kari <kari.lynn.dreyer@gmail.com>
2024-04-11 12:07:12 +0200

    Update comments for clarity

--- js/scripts.js
+++ js/scripts.js
@@ -12,22 +12,40 @@ let pokemonRepository = (function () {
     {name: 'Blastoise', height: 1.6, types: ['water']},
   ];
 
+  // Function to make a list of all Pokemon in the repository
   function getAll () {
     return pokemonList
   }
 
+  //Function to add a Pokemon to the repository
   function add (pokemon) {
     pokemonList.push(pokemon);
   }
 
+  //Function to create a list item in the DOM for each Pokemon
   function addListItem (pokemon) {
+
+    //Identify a variable for the ul "pokemon-list" in index.html
     let pokemonList = document.querySelector('.pokemon-list');
+    
+    //Create list item element
     let listItem = document.createElement('li');
+
+    //Create a button with the pokemon's name and give it the class "button" for CSS styling
     let button = document.createElement('button');
     button.innerText = pokemon.name;
     button.classList.add('button-class');
+
+    //Append one of the above buttons to each of the above list items
     listItem.appendChild(button);
+
+    //Append the list item to the DOM within the ul "pokemon-list"
     pokemonList.appendChild(listItem);
+  }
+
+   //Function to display Pokemon Details
+   function showDetails(pokemon) {
+    console.log(pokemon);
   }
 
   return {

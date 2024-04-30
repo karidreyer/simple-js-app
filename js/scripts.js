f950d7ca9cebd143dfb0ab0e8dc3598d7747c8ec
Kari <kari.lynn.dreyer@gmail.com>
2024-04-30 13:15:55 +0200

    Add "back to top" button and functionality

--- js/scripts.js
+++ js/scripts.js
@@ -110,3 +110,19 @@ pokemonRepository.loadList().then(function() {
   });
 })
 
+//Code for "Back to top" button functionality
+let backToTopBtn = document.getElementById('back-to-top-btn');
+
+window.addEventListener('scroll', () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn.style.display = 'block';
+  } else {
+    backToTopBtn.style.display = 'none';
+  }
+});
+
+backToTopBtn.addEventListener('click', () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
